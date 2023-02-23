3b56bdb2f466a98e82ea028c632ee6241fe1a2be
mikk111111 <mik.k00259@gmail.com>
2023-02-23 22:31:15 +0200

    done with downgrade

--- MainUniProject/MyHeader.h
+++ MainUniProject/MyHeader.h
@@ -9,3 +9,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <random>
+
+struct student//DONE
+{
+	std::string studName;
+	std::string studSurname;
+	std::vector<int> marks;
+	int finalMark;
+};
+
+std::random_device dev;
+std::mt19937 rng(dev());
+std::uniform_int_distribution<std::mt19937::result_type> dist0_10(0, 10); // distribution in range
+std::uniform_int_distribution<std::mt19937::result_type> dist1_10(1, 10); // distribution in range
