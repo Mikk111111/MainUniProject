c986a2c739d585c2fa6e578eccfca1c82495653c
mikk111111 <mik.k00259@gmail.com>
2023-03-03 01:03:14 +0200

    Done with reading and writing

--- MainUniProject/MyHeader.h
+++ MainUniProject/MyHeader.h
@@ -1,14 +1,17 @@
 #pragma once
-#include <iostream>
-#include <fstream>
-#include <vector>
 #include <algorithm>
+#include <chrono>
+#include <fstream>
 #include <iomanip>
-#include <string>
+#include <iostream>
+#include <random>
+#include <sstream> 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 #include <time.h>
-#include <random>
+#include <vector>
+//#include <boost/algorithm/string.hpp>
 
 struct student//DONE
 {
@@ -18,7 +21,22 @@ struct student//DONE
 	int finalMark;
 };
 
+student addStudent();
+void printWithMed(student stud);
+void printWithAvg(student stud);
+void printBoth(student stud);
+student randFillStudents(int studPosition);
+void menu(std::vector<student> studentBody);
+
 std::random_device dev;
 std::mt19937 rng(dev());
 std::uniform_int_distribution<std::mt19937::result_type> dist0_10(0, 10); // distribution in range
 std::uniform_int_distribution<std::mt19937::result_type> dist1_10(1, 10); // distribution in range
+
+void bufferRead(std::string readName, std::vector<student> &studentBody);
+void fillText(std::string writeName, std::vector<student> studentBody);
+
+bool studentSorter(student const& studB, student const& studE) {
+    if (studB.studName != studE.studName)
+        return studB.studName < studE.studName;
+}
--- MainUniProject/Source.cpp
+++ MainUniProject/Source.cpp
@@ -1,22 +1,28 @@
 #include "MyHeader.h"
 
-student addStudent();
-void printWithMed(student stud);//DONE
-void printWithAvg(student stud);//DONE
-student randFillStudents(int studPosition);
+int main()
+{
+	std::vector<student> studs;
+	menu(studs);
+	return 0;
+}
 
-void menu(std::vector<student> studentBody)//TODO
+void menu(std::vector<student> studentBody) //TODO
 {
 	std::string commandS;
 	int command = 0;
 
 	while (true)
 	{
-		std::cout << "1. Print Student List with Median" << std::endl;
-		std::cout << "2. Print Student List with Average" << std::endl;
-		std::cout << "3. Add Student" << std::endl;
-		std::cout << "4. Create random students" << std::endl;
-		std::cout << "5. End Work" << std::endl;
+		std::cout << "1. Print Student List and Marks with Median" << std::endl;
+		std::cout << "2. Print Student List and Marks with Average" << std::endl;
+		std::cout << "3. Print Student List only with Average and Median" << std::endl;
+		std::cout << "4. Add Student" << std::endl;
+		std::cout << "5. Create random students" << std::endl;
+		std::cout << "6. Read from file" << std::endl;
+		std::cout << "7. Write to file" << std::endl;
+		std::cout << "8. End Work" << std::endl;
+		std::cout << "Enter Command:";
 		std::cin >> commandS;
 		try
 		{
@@ -27,23 +33,26 @@ void menu(std::vector<student> studentBody)//TODO
 			command = 0;
 			std::cout << "Unidentified command, try again" << std::endl;
 		}
-
 		switch (command)
 		{
-			case 1://print med
-				for (const student& std : studentBody)
-					printWithMed(std);
-				break;
-			case 2://print avg
-				for (const student& std : studentBody)
-					printWithAvg(std);
-				break;
-			case 3://add stud
-				studentBody.push_back(addStudent());
-				break;
-			case 4://generate stud
+		case 1: //print med
+			for (const student& std : studentBody)
+				printWithMed(std);
+			break;
+		case 2: //print avg
+			for (const student& std : studentBody)
+				printWithAvg(std);
+			break;
+		case 3:
+			for (const student& std : studentBody)
+				printBoth(std);
+			break;
+		case 4: //add stud
+			studentBody.push_back(addStudent());
+			break;
+		case 5: //generate stud
 			{
-					std::string amountS;
+				std::string amountS;
 				int amount;
 				std::cout << "Generate how many?:";
 				std::cin >> amountS;
@@ -59,35 +68,39 @@ void menu(std::vector<student> studentBody)//TODO
 				for (int i = 0; i <= amount; i++)
 					studentBody.push_back(randFillStudents(i + 1));
 			}
-				break;
-			case 5:
-				return;//end work
-			default:
-				std::cout << "-----------------------------------------------" << std::endl;
+			break;
+		case 6:
+			{
+				std::string readFileName;
+				std::cout << "Enter File Name:";
+				std::cin >> readFileName;
+				bufferRead(readFileName, studentBody);
+			}
+			break;
+		case 7:
+			fillText("test.txt", studentBody);
+			std::cout << "Done" << std::endl;
+			break;
+		case 8:
+			return; //end work
+		default:
+			std::cout << "-----------------------------------------------" << std::endl;
 			break;
 		}
 	}
-	
 }
 
-int main()
-{
-	std::vector<student> studs;
-	menu(studs);
-	return 0;
-}
-
-student addStudent()//TODO
+student addStudent() //TODO
 {
 	student temp;
-	
+
 	std::vector<int> tempArr;
 	int tempo1 = 0;
 	std::string tempo2;
 	int amount;
 	std::cout << "Enter Student Name: ";
 	std::cin >> temp.studName;
-	std::cout <<std::endl<< "Enter Student Surname: ";
+	std::cout << std::endl << "Enter Student Surname: ";
 	std::cin >> temp.studSurname;
 	while (true)
 	{
@@ -102,7 +115,7 @@ student addStudent()//TODO
 			}
 			else
 			{
-				temp.finalMark=tempo1;
+				temp.finalMark = tempo1;
 				break;
 			}
 		}
@@ -126,10 +139,9 @@ student addStudent()//TODO
 		}
 	}
 
-	while(amount>0)
+	while (amount > 0)
 	{
-		
-		std::cout << "Enter " << tempArr.size()+1 << " Mark:";
+		std::cout << "Enter " << tempArr.size() + 1 << " Mark:";
 		std::cin >> tempo2;
 		try
 		{
@@ -144,45 +156,68 @@ student addStudent()//TODO
 				amount--;
 			}
 		}
-		catch(std::exception e)
+		catch (std::exception e)
 		{
 			std::cout << "Try again" << std::endl;
 		}
-		
 	}
 	temp.marks = tempArr;
 	return temp;
 }
 
-void printWithAvg(student stud)//DONE
+void printBoth(student stud)
 {
-	std::cout << "Student "<<std::setw(15) << stud.studName << " " << stud.studSurname << " Marks: ";
-	double temp=0;
-	for(const int &mark:stud.marks)
+	std::cout << "Student| " << std::setw(22) << stud.studName << "|" << std::setw(22) << stud.studSurname <<
+		"|";
+	double temp = 0;
+	for (const int& mark : stud.marks) temp += mark;
+	std::cout << ", Average: " << std::setprecision(3) << std::setw(5) << temp / stud.marks.size() << "| ";
+	if (stud.marks.size() % 2 == 0) //even
 	{
-		std::cout << "[" << mark << "]";
+		std::sort(stud.marks.begin(), stud.marks.end());
+		std::cout << ", Median: " << std::setprecision(3) << std::setw(5)
+		<< (stud.marks[stud.marks.size() / 2] + stud.marks[stud.marks.size() / 2 - 1]) / 2 << "| ";
+	}
+	else //odd
+	{
+		std::sort(stud.marks.begin(), stud.marks.end());
+		std::cout << ", Median: " << std::setw(5) << stud.marks[stud.marks.size() / 2] << "| ";
+	}
+	std::cout << std::endl;
+}
+
+void printWithAvg(student stud) //DONE
+{
+	std::cout << "Student " << std::setw(22) << stud.studName << "|" << std::setw(22) << stud.studSurname <<
+		"| Marks: ";
+	double temp = 0;
+	for (const int& mark : stud.marks)
+	{
+		std::cout << "[" << std::setw(2) << mark << "]";
 		temp += mark;
 	}
-	std::cout << ", Average: " <<std::setprecision(2)<< temp / stud.marks.size();
+	std::cout << ", Average: " << std::setprecision(3) << std::setw(5) << temp / stud.marks.size();
 	std::cout << ", Final Mark: " << stud.finalMark << std::endl;
 }
 
-void printWithMed(student stud)//DONE
+void printWithMed(student stud) //DONE
 {
-	std::cout << "Student " << stud.studName << " " << stud.studSurname << " Marks: ";
+	std::cout << "Student " << std::setw(22) << stud.studName << "|" << std::setw(22) << stud.studSurname <<
+		"| Marks: ";
 	for (const int& mark : stud.marks)
 	{
-		std::cout << "[" << mark << "]";
+		std::cout << "[" << std::setw(2) << mark << "]";
 	}
-	if(stud.marks.size()%2==0)//even
-	{
-		std::sort(stud.marks.begin(),stud.marks.end());
-		std::cout << ", Median: " << std::setprecision(2) << (stud.marks[stud.marks.size()/2]+stud.marks[stud.marks.size()/2-1])/2;
-	}
-	else//odd
+	if (stud.marks.size() % 2 == 0) //even
 	{
 		std::sort(stud.marks.begin(), stud.marks.end());
-		std::cout << ", Median: " << stud.marks[stud.marks.size()/2];
+		std::cout << ", Median: " << std::setprecision(3) << std::setw(5) << (stud.marks[stud.marks.size() / 2] + stud.marks[stud.marks.
+			size() / 2 - 1]) / 2;
+	}
+	else //odd
+	{
+		std::sort(stud.marks.begin(), stud.marks.end());
+		std::cout << ", Median: " << stud.marks[stud.marks.size() / 2];
 	}
 	std::cout << ", Final Mark: " << stud.finalMark << std::endl;
 }
@@ -190,17 +225,106 @@ void printWithMed(student stud)//DONE
 student randFillStudents(int studPosition)
 {
 	student temp;
-	std::vector<int> tempArr;
-	for(int i = 0; i < dist1_10(rng);i++)
-	{
-		int tempmark = dist0_10(rng);
-		tempArr.push_back(tempmark);
-	}
-
-	temp.studName = "Vardenis" +std::to_string(studPosition);
+	for (int i = 0; i < static_cast<unsigned>(dist1_10(rng)); i++)
+		temp.marks.push_back(dist0_10(rng));
+	temp.studName = "Vardenis" + std::to_string(studPosition);
 	temp.studSurname = "Pavardenis" + std::to_string(studPosition);
 	temp.finalMark = dist0_10(rng);
-	temp.marks = tempArr;
-
 	return temp;
+}
+
+void bufferRead(std::string readName, std::vector<student>& studentBody)
+{
+	std::vector<std::string> splited;
+	std::string line;
+	std::stringstream my_buffer;
+	
+	std::ifstream open_f(readName); //Open file
+	if (open_f)
+	{
+		my_buffer << open_f.rdbuf(); //Load to buffer
+	}
+	else
+	{
+		std::cout << "Couldn't find the file" << std::endl;
+		return;
+	}
+	open_f.close(); //Close file
+
+	while (my_buffer)
+	{
+		if (!my_buffer.eof())
+		{
+			std::getline(my_buffer, line);
+			splited.push_back(line);
+		}
+		else
+		{
+			break;
+		}
+	}
+	splited.erase(splited.begin());
+	for (std::string& fullLine : splited)
+	{
+		student temp;
+		temp.marks.clear();
+		int readPlace = 0;
+		int numb;
+		std::istringstream ss(fullLine);
+		std::string word; // for storing each word
+		if (!fullLine.empty())
+		{
+			while (ss >> word)
+			{
+				readPlace++;
+				if (readPlace == 1)temp.studName = word;
+				else if (readPlace == 2)temp.studSurname = word;
+				else
+				{
+					try
+					{
+						temp.marks.push_back(std::stoi(word));
+					}
+					catch (...)
+					{
+						std::cout << "Failed to convert marks" << std::endl;
+					}
+				}
+			}
+			temp.finalMark = temp.marks.back();
+			temp.marks.pop_back();
+			studentBody.push_back(temp);
+		}
+	}
+	splited.clear();
+}
+
+void fillText(std::string writeName, std::vector<student> studentBody)
+{
+	std::ofstream out(writeName);
+	std::string outText;
+	std::sort(studentBody.begin(), studentBody.end(),&studentSorter );
+	for (auto temp : studentBody)
+	{
+		out << std::setw(20) << temp.studName << std::setw(20) << temp.studSurname;
+		
+		double tempor = 0;
+		for (const int& mark : temp.marks) tempor += mark;
+		out << ", Average: " << std::setprecision(3) << std::setw(5) << tempor / temp.marks.size() << "| ";
+		if (temp.marks.size() % 2 == 0) //even
+		{
+			std::sort(temp.marks.begin(), temp.marks.end());
+			out << ", Median: " << std::setprecision(3) << std::setw(5) << 
+				(temp.marks[temp.marks.size() / 2] + temp.marks[temp.marks.size() / 2 - 1]) / 2 << "| ";
+		}
+		else //odd
+		{
+			std::sort(temp.marks.begin(), temp.marks.end());
+			out << ", Median: " << std::setw(5) << temp.marks[temp.marks.size() / 2] << "| ";
+		}
+		out << std::endl;
+			
+	}
+
+	out.close();
 }
