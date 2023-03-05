cb229a20533e6c9dfe3aae3b42d825b62fb7e7d3
mikk111111 <mik.k00259@gmail.com>
2023-03-05 15:19:08 +0200

    rewriting fro dinamic arrays

--- MainUniProject/MyHeader.h
+++ MainUniProject/MyHeader.h
@@ -22,3 +22,9 @@ std::random_device dev;
 std::mt19937 rng(dev());
 std::uniform_int_distribution<std::mt19937::result_type> dist0_10(0, 10); // distribution in range
 std::uniform_int_distribution<std::mt19937::result_type> dist1_10(1, 10); // distribution in range
+
+void menuArr();//DONE
+void printWithAvgArr(student* &students, int place);//DONE
+void printWithMedArr(student* &students, int place);//DONE
+void addStudentArr(student* &students, int place);//DONE
+void randFillStudentsArr(student* &students, int place);//DONE
