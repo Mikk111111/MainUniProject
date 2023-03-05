00648d1c643fd3c618848a581ae77e82ef77a394
Mikk <70907877+Mikk111111@users.noreply.github.com>
2023-03-06 00:16:46 +0200

    Update MyHeader.h

--- MainUniProject/MyHeader.h
+++ MainUniProject/MyHeader.h
@@ -9,12 +9,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <random>
+#include <cstdlib>
 
 struct student//DONE
 {
 	std::string studName;
 	std::string studSurname;
-	std::vector<int> marks;
+	int* marks;
 	int finalMark;
 };
 
@@ -23,7 +24,7 @@ std::mt19937 rng(dev());
 std::uniform_int_distribution<std::mt19937::result_type> dist0_10(0, 10); // distribution in range
 std::uniform_int_distribution<std::mt19937::result_type> dist1_10(1, 10); // distribution in range
 
-void menuArr();//DONE
+void menuArr(student* &students, int& studentAmount);//DONE
 void printWithAvgArr(student* &students, int place);//DONE
 void printWithMedArr(student* &students, int place);//DONE
 void addStudentArr(student* &students, int place);//DONE
